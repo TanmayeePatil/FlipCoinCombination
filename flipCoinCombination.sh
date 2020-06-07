3ef9fc2acde07a455f583bf1af1c0eb01f6bfa72
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-07 19:38:49 +0530

    Added: Random function to generate winner head or tail

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "WELCOM TO FLIP COIN COMBINATION PROBLEM"
+echo "WELCOME TO FLIP COIN COMBINATION PROBLEM"
+
+myRan=$(( ( RANDOM % 2 ) + 1 ))
+
+if [ $myRan -eq 1 ];then
+   echo "HEADS"
+else
+   echo "TAILS"
+fi
