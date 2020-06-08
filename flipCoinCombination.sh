bc434d14b640f4fc353bc1143ebba192abd6b612
TanmayeePatil <tanmayeepatil@outlook.com>
2020-06-08 08:14:52 +0530

    Added: caluclated triplet percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -10,6 +10,8 @@ i=0
 
 read -p "How many time you want to flip coin: " filpTimes
 
+orgFlipTimes=$filpTimes
+
 while [ $i -ne $filpTimes ]
 do
 myRan=$(( ( RANDOM % 2 ) + 1 ))
@@ -50,7 +52,7 @@ countHH=0
 countHT=0
 countTH=0
 countTT=0
-filpTimes=$(( $filpTimes*2 ))
+filpTimes=$(( $filpTimes+$orgFlipTimes ))
 
 while [ $i -ne $filpTimes ]
 do
@@ -88,14 +90,96 @@ echo "Count of HT: $countHT"
 echo "Count of TH: $countTH"
 echo "Count of TT: $countTT"
 
-singleHHPer=`echo $countHH $filpTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
-singleHTPer=`echo $countHT $filpTimes | awk -F" " '{ x=$1; y=$2; res2=(x/y*100); print res2 }'`
-singleTHPer=`echo $countTH $filpTimes | awk -F" " '{ p=$1; q=$2; res3=(p/q*100); print res3 }'`
-singleTTPer=`echo $countTT $filpTimes | awk -F" " '{ o=$1; m=$2; res4=(o/m*100); print res4 }'`
+doubletHHPer=`echo $countHH $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+doubletHTPer=`echo $countHT $orgFlipTimes | awk -F" " '{ x=$1; y=$2; res2=(x/y*100); print res2 }'`
+doubletTHPer=`echo $countTH $orgFlipTimes | awk -F" " '{ p=$1; q=$2; res3=(p/q*100); print res3 }'`
+doubletTTPer=`echo $countTT $orgFlipTimes | awk -F" " '{ o=$1; m=$2; res4=(o/m*100); print res4 }'`
 
 echo ""
-echo "Percentage HH: $singleHHPer"
-echo "Percentage HT: $singleHTPer"
-echo "Percentage TH: $singleTHPer"
-echo "Percentage TT: $singleTTPer"
+echo "Percentage HH: $doubletHHPer"
+echo "Percentage HT: $doubletHTPer"
+echo "Percentage TH: $doubletTHPer"
+echo "Percentage TT: $doubletTTPer"
+echo "-------------------------------------------------"
+
+################# TRIPLET COIN FLIP #####################
+countTTT=0
+countHTT=0
+countTHT=0
+countTTH=0
+countHTT=0
+countHTH=0
+countTHH=0
+countHHH=0
+filpTimes=$(( $filpTimes+$orgFlipTimes ))
+
+while [ $i -ne $filpTimes ]
+do
+myRan=$(( (RANDOM % 8) + 1 ))
+ if [ $myRan -eq 1 ];then
+   FilpCoinDict[$i]="TTT"
+   ((countTTT++))
+ elif [ $myRan -eq 2 ];then
+   FilpCoinDict[$i]="HTT"
+   ((countHTT++))
+ elif [ $myRan -eq 3 ];then
+   FilpCoinDict[$i]="THT"
+   ((countTHT++))
+ elif [ $myRan -eq 4 ];then
+   FilpCoinDict[$i]="TTH"
+   ((countTTH++))
+ elif [ $myRan -eq 5 ];then
+   FilpCoinDict[$i]="HTT"
+   ((countHTT++))
+ elif [ $myRan -eq 6 ];then
+   FilpCoinDict[$i]="HTH"
+   ((countHTH++))
+ elif [ $myRan -eq 7 ];then
+   FilpCoinDict[$i]="THH"
+   ((countTHH++))
+ else
+  FilpCoinDict[$i]="HHH"
+   ((countHHH++))
+ fi
+   (( i++ ))
+done
+
+echo ""
+echo "DICTONARY RECORDS:"
+echo "-------------------------------------------------"
+for key in "${!FilpCoinDict[@]}"; do
+  printf '%s:%s\n' "$key" "${FilpCoinDict[$key]}"
+done | sort -t : -k 2n
+echo "-------------------------------------------------"
+
+echo ""
+echo "TRIPLET RESULT: "
+echo "-------------------------------------------------"
+echo "Count of TTT: $countTTT"
+echo "Count of HTT: $countHTT"
+echo "Count of THT: $countTHT"
+echo "Count of TTH: $countTTH"
+echo "Count of HTT: $countHTT"
+echo "Count of HTH: $countHTH"
+echo "Count of THH: $countTHH"
+echo "Count of HHH: $countHHH"
+
+tripletTTTPer=`echo $countTTT $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+tripletHTTPer=`echo $countHTT $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+tripletTHTPer=`echo $countTHT $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+tripletTTHPer=`echo $countTTH $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+tripletHTTPer=`echo $countHTT $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+tripletHTHPer=`echo $countHTH $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+tripletTHHPer=`echo $countTHH $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+tripletHHHPer=`echo $countHHH $orgFlipTimes | awk -F" " '{ a=$1; b=$2; res1=(a/b*100); print res1 }'`
+
+echo ""
+echo "Percentage TTT: $tripletTTTPer"
+echo "Percentage HTT: $tripletHTTPer"
+echo "Percentage THT: $tripletTHTPer"
+echo "Percentage TTH: $tripletTTHPer"
+echo "Percentage HTT: $tripletHTTPer"
+echo "Percentage HTH: $tripletHTHPer"
+echo "Percentage THH: $tripletTHHPer"
+echo "Percentage HHH: $tripletHHHPer"
 echo "-------------------------------------------------"
